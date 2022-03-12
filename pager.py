05c51bb094cb649e08c9f2aa97c4a030edd61c5b
Yoan Ivanov <54147006+y0608@users.noreply.github.com>
2022-03-12 11:08:02 +0200

    Working pager - sending and recieving

--- pager.py
+++ pager.py
@@ -1,18 +1,30 @@
 import socket
+from _thread import *
 ClientMultiSocket = socket.socket()
 host = '192.168.1.20'
 port = 2004
+
 print('Waiting for connection response')
+
 try:
     ClientMultiSocket.connect((host, port))
 except socket.error as e:
     print(str(e))
+
 res = ClientMultiSocket.recv(1024)
-while True:
-    Input = input('Hey there: ') 
-    #Input1 = input('test: ')
-    #print(Input, Input1)
-    ClientMultiSocket.send(str.encode(Input))
-    res = ClientMultiSocket.recv(1024)
-    print(res.decode('utf-8'))
+
+def send_msg():
+    while True:
+        Input = input('Hey there: ') 
+        ClientMultiSocket.send(str.encode(Input))
+    
+def resive_msg():
+    while True:
+        res = ClientMultiSocket.recv(1024)
+        print(res.decode('utf-8'))
+
+
+start_new_thread(resive_msg, ())
+send_msg()
+
 ClientMultiSocket.close()
