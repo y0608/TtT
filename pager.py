c9c173018983e445285c65b87ff4c7f77f143fe9
Yoan Ivanov <54147006+y0608@users.noreply.github.com>
2022-03-12 10:26:11 +0200

    old pager, but recieving

--- pager.py
+++ pager.py
@@ -1,37 +1,18 @@
 import socket
-import os
-from _thread import *
-ServerSideSocket = socket.socket()
-host = '192.168.1.33'
+ClientMultiSocket = socket.socket()
+host = '192.168.1.20'
 port = 2004
-ThreadCount = 0
-
+print('Waiting for connection response')
 try:
-    ServerSideSocket.bind((host, port))
+    ClientMultiSocket.connect((host, port))
 except socket.error as e:
     print(str(e))
-print('Socket is listening..')
-ServerSideSocket.listen(5)
-
-
-def multi_threaded_client(connection):
-    connection.send(str.encode('Server is working:'))
-    while True:
-        data = connection.recv(2048)
-        response = 'Server message: ' + data.decode('utf-8')
-        if not data:
-            break
-
-        print(str(connection) +  "---->" + response)
-        
-        connection.sendall(str.encode(response[0]))
-    connection.close()
-
-
+res = ClientMultiSocket.recv(1024)
 while True:
-    Client, address = ServerSideSocket.accept()
-    print('Connected to: ' + address[0] + ':' + str(address[1]))
-    start_new_thread(multi_threaded_client, (Client, ))
-    ThreadCount += 1
-    print('Thread Number: ' + str(ThreadCount))
-ServerSideSocket.close()
+    Input = input('Hey there: ') 
+    #Input1 = input('test: ')
+    #print(Input, Input1)
+    ClientMultiSocket.send(str.encode(Input))
+    res = ClientMultiSocket.recv(1024)
+    print(res.decode('utf-8'))
+ClientMultiSocket.close()
